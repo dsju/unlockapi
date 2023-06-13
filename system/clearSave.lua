ef9ca1c1ad5031ffcc341a7eb945af85b9326781
dsju <135464939+dsju@users.noreply.github.com>
2023-06-13 10:12:53 +0300

    Delete clearSave.lua

--- system/clearSave.lua
+++
@@ -1,5 +0,0 @@
-function UnlockAPI.Callback:ClearSaveData()
-    UnlockAPI.Save = { Characters = {}, Challenges = {} }
-end
-
-UnlockAPI.Mod:AddPriorityCallback(ModCallbacks.MC_POST_GAME_STARTED, UnlockAPI.Enums.CallbackPriority.EARLIEST, UnlockAPI.Callback.ClearSaveData)
