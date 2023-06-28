5df6f7e06512ab64209e567aa8111946f7771a8d
dsju <135464939+dsju@users.noreply.github.com>
2023-06-28 09:29:57 +0300

    Fix another bug

--- enums.lua
+++ enums.lua
@@ -50,8 +50,8 @@ UnlockAPI.Enums = {
         AFTER_EARLIEST = -2^30 + 1,
     },
 
-    RandomPopupPreventionAchievement = {{
+    RandomPopupPreventionAchievement = {{{
         UnlockRequirements = "So guys, we did it, we reached a quarter of a million subscribers, 250,000 subscribers and still growing. The fact that we've reached this number in such a short amount of time is just phenomenal, I'm- I'm just amazed. Thank you all so much for supporting this channel and helping it grow. I- I love you guys... You guys are just awesome.",
         AchievementGfx = nil,
-    }}
+    }}}
 }
